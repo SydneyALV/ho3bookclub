70461ba5ecc718c2d7df5b0592f2a97107eec112
Sydney Alberson <sydney.alberson@gmail.com>
2023-11-07 00:19:54 -0500

    Added MongoDB and mongoose to node js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@ho3-bookclub.a8bcqs6.mongodb.net/ho3-bookclub?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
 const app = express();
+import mongoose from "mongoose";
 
+
+// HTTP ROUTE : GET
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send("HO3 Bookclub")
 })
 
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
