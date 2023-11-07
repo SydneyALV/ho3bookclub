768f15f525396562b78d91960cf1e25e7f2c235d
Sydney Alberson <sydney.alberson@gmail.com>
2023-11-07 00:51:43 -0500

    Created HTTP route to post new book

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,46 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js"
 const app = express();
 import mongoose from "mongoose";
+import { Book } from "./models/BookModel.js";
 
+// Middleware for parsing request body
+app.use(express.json());
 
 // HTTP ROUTE : GET
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send("Welcome to HO3 Bookclub")
 })
+
+// HTTP ROUTE : POST
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Must enter all required fields: Title, Author, Publishing Year"
+            })
+        }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        }
+
+        const book = await Book.create(newBook);
+        return response.status(201).send({ book })
+    
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
+})
+
+
 
 mongoose
     .connect(mongoDBURL)
