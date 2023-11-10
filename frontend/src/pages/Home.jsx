ab50017df01e77da3b48527940674fc8a6a03452
Sydney Alberson <sydney.alberson@gmail.com>
2023-11-10 11:43:35 -0500

    Put table in separate component and added to home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs'
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import BooksCard from '../components/home/BooksCard';
+import BooksTable from '../components/home/BooksTable';
+
 
 const Home = () => {
   const [books, setBooks] = useState([1]);
@@ -24,8 +27,6 @@ const Home = () => {
       });
   }, []);
 
-  books.map(b => console.log(b.title))
-
   return (
     <div className='p-4'>
       <div className='flex justify-between items-center'>
@@ -33,56 +34,10 @@ const Home = () => {
           Books List
         </h1>
         <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4x1' />
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
-
-      {loading ? (
-        <Spinner />
-      ) : (
-        <table className='w-full border-separate border-spacing-2'>
-          <thead>
-            <tr>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>No</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Title</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
-              <th className='border border-slate-600 rounded-md'>Operations</th>
-            </tr>
-          </thead>
-          <tbody className="border">
-            {books.map((book, index) => (
-              <tr className='h-8' key={book._id}>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  {index + 1}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  {book.title}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                  {book.author}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                  {book.publishYear}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  <div className='flex justify-center gap-x-4'>
-                    <Link to={`/books/details/${book._id}`}>
-                      <BsInfoCircle className='text-2xl text-green-800' />
-                    </Link>
-                    <Link to={`/books/edit/${book._id}`}>
-                      <AiOutlineEdit className='text-2xl text-yellow-600' />
-                    </Link>
-                    <Link to={`/books/delete/${book._id}`}>
-                      <MdOutlineDelete className='text-2xl text-red-600' />
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      {loading ? <Spinner /> : <BooksTable books={books}/>}
     </div>
   )
 }
