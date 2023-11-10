725986ccb11f1e491caee3de947dccd42484b968
Sydney Alberson <sydney.alberson@gmail.com>
2023-11-10 12:41:39 -0500

    Made BookCard component and allowed option in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs'
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import { MdOutlineAddBox } from 'react-icons/md'
 import BooksCard from '../components/home/BooksCard';
 import BooksTable from '../components/home/BooksTable';
 
@@ -12,6 +10,7 @@ import BooksTable from '../components/home/BooksTable';
 const Home = () => {
   const [books, setBooks] = useState([1]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('card');
 
   useEffect(() => {
     setLoading(true);
@@ -29,6 +28,20 @@ const Home = () => {
 
   return (
     <div className='p-4'>
+      <div className='flex justify-center items-center gap-x-4'>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          onClick={() => setShowType('card')}
+        >
+          Card
+        </button>
+      </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>
           Books List
@@ -37,7 +50,15 @@ const Home = () => {
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
-      {loading ? <Spinner /> : <BooksTable books={books}/>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        showType === 'table' ? (
+          <BooksTable books={books} />
+        ) : (
+          <BooksCard books={books} />
+        )
+      )}
     </div>
   )
 }
