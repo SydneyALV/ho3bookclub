8061fb0edc5e95362ea6e9ac47ca6636b32a1006
Sydney Alberson <sydney.alberson@gmail.com>
2023-11-15 14:57:43 -0500

    Created navigation and authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,30 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn
+} from "@clerk/clerk-react"
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPubKey) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={clerkPubKey}>
+    <SignedIn>
+      <BrowserRouter>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </BrowserRouter>,
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </ClerkProvider>
 )
